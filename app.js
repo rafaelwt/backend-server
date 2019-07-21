f9ab6dec38e188dc8b4ecd050ba548ca05f65e8a
Rafael <rafael.wt@gmail.com>
2019-07-21 18:03:34 -0400

    hospitales seccion 11

--- app.js
+++ app.js
@@ -2,32 +2,57 @@
 var express = require('express');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
-// Inicializar Variables
+
+// Inicializar variables
 var app = express();
 
+
 // Body Parser
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json());
-
-// importar rutas;
 // parse application/x-www-form-urlencoded
-var appRoutes = require('./routes/app');
-var usuarioRoutes =  require('./routes/usuario');
-var loginRoutes = require('./routes/login');
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
-// Conexion a la base de datos
-mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB',(err, res)=> {
-    if(err) throw err;
+
+// Importar rutas
+var appRoutes = require('./routes/app');
+var usuarioRoutes = require('./routes/usuario');
+var loginRoutes = require('./routes/login');
+var hospitalRoutes = require('./routes/hospital');
+var medicoRoutes = require('./routes/medico');
+var busquedaRoutes = require('./routes/busqueda');
+var uploadRoutes = require('./routes/upload');
+var imagenesRoutes = require('./routes/imagenes');
+
+
+// Conexión a la base de datos
+mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, res) => {
+
+    if (err) throw err;
+
     console.log('Base de datos: \x1b[32m%s\x1b[0m', 'online');
+
 });
 
+// Server index config
+// var serveIndex = require('serve-index');
+// app.use(express.static(__dirname + '/'))
+// app.use('/uploads', serveIndex(__dirname + '/uploads'));
 
-// Rutas // middleware para que apunte al archivo routes
+
+
+// Rutas
 app.use('/usuario', usuarioRoutes);
+app.use('/hospital', hospitalRoutes);
+app.use('/medico', medicoRoutes);
 app.use('/login', loginRoutes);
+app.use('/busqueda', busquedaRoutes);
+app.use('/upload', uploadRoutes);
+app.use('/img', imagenesRoutes);
+
 app.use('/', appRoutes);
 
-// Escuchar Peticiones
-app.listen(3000, ()=> {
+
+// Escuchar peticiones
+app.listen(3000, () => {
     console.log('Express server puerto 3000: \x1b[32m%s\x1b[0m', 'online');
 });
